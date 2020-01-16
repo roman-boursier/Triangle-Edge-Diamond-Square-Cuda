bf5f718a470f92e06e25456f1b7c6bd93ad1c50b
roman-boursier <roman.boursier@gmail.com>
2020-01-16 23:15:53 +0100

    bugFix on image generation

--- functions.cu
+++ functions.cu
@@ -203,44 +203,24 @@ void diamontCPU(int w, int h, unsigned char * data) {
 	}
 }
 
-__global__ void diamont(unsigned char * data, unsigned char * tabDepth, int4 * _tabParents, int i, int tailleTab) {
-	int thx = blockIdx.x * blockDim.x + threadIdx.x;
-	int thy = blockIdx.y * blockDim.y + threadIdx.y;
-	int ThId = thy * tailleTab + thx;
-	int nbPar = 0;
-
-	if(tabDepth[ThId] == i  && i != 1) {
-
-		if(_tabParents[ThId].x != -1) nbPar ++;
-		if(_tabParents[ThId].y != -1) nbPar ++;
-		if(_tabParents[ThId].z != -1) nbPar ++;
-		if(_tabParents[ThId].w != -1) nbPar ++;
-		
-		data[ThId] = (data[_tabParents[ThId].x] + data[_tabParents[ThId].y] + data[_tabParents[ThId].z] + data[_tabParents[ThId].w]);
-		
-		(nbPar == 0) ? data[ThId] = 1 : data[ThId] /= nbPar;
-	}
-}
-
 __global__ void diamontImg(unsigned char * data, unsigned char * img, unsigned char * tabDepth, int4 * _tabParents, int i, int tailleTab) {
 	int thx = blockIdx.x * blockDim.x + threadIdx.x;
 	int thy = blockIdx.y * blockDim.y + threadIdx.y;
 	int ThId = thy * tailleTab + thx;
 	int nbPar = 0;
 
-	if(tabDepth[ThId] == i  && i != 1) {
-
+	if(data[ThId] == 0 && tabDepth[ThId] == i  && i != 1) {
+	
 		if(_tabParents[ThId].x != -1) nbPar ++;
 		if(_tabParents[ThId].y != -1) nbPar ++;
 		if(_tabParents[ThId].z != -1) nbPar ++;
 		if(_tabParents[ThId].w != -1) nbPar ++;
 		
-		data[ThId] = (data[_tabParents[ThId].x] + data[_tabParents[ThId].y] + data[_tabParents[ThId].z] + data[_tabParents[ThId].w]);
-		
-		(nbPar == 0) ? data[ThId] = 1 : data[ThId] /= nbPar;
+		data[ThId] = (data[_tabParents[ThId].x] + data[_tabParents[ThId].y] + data[_tabParents[ThId].z] + data[_tabParents[ThId].w]) / nbPar;
 
-		img[ThId] = 255 / data[ThId];
+		img[ThId] = data[ThId];
 	}
+	
 }
 
 void initTime(void) {
--- main.cu
+++ main.cu
@@ -31,22 +31,28 @@ int main(int argc, char ** argv){
 	unsigned char *data, *devData, *tabDepth, *devTabDepth;
 	int4 *_tabParents, *dev_tabParents;
 	
-	//Img output
-	Mat img(matDim, matDim, CV_8UC1);
-	const int grayBytes = img.step * img.rows;
-	unsigned char *d_input, *d_img;
-
 	/*Init data*/
 	data = (unsigned char *) calloc(matDim * matDim, sizeof *data);
 	data[0] = data[matDim - 1] = data[ (matDim - 1) * matDim] = data[matDim*matDim - 1] = 1;
-    data[matDim / 2 * matDim + matDim / 2] = matDim / 2 + 1;
+    data[matDim / 2 * matDim + matDim / 2] = 255;
 
 	/*-------------CPU MODE--------------------*/
 
 	if(strcmp(mode,cpu) == 0){
-		initTime();
+		//initTime();
+
 		diamontCPU(matDim, matDim, data);
-		printf("%d\t%F\n", n, getTime());
+
+		//Img
+		Mat img(matDim, matDim, CV_8UC1, Scalar(0));
+		for(int y = 0; y < matDim; y ++){
+			for(int x = 0; x < matDim; x ++){
+				img.at<uchar>(y,x) = data[y * matDim + x];
+			}
+		}
+		//makprintf("%d\t%F\n", n, getTime());
+
+		cv::imwrite(filename, img);
 	}
 
 	/*-------------GPU MODE--------------------*/
@@ -58,46 +64,57 @@ int main(int argc, char ** argv){
 		_tabParents = (int4 *) calloc(matDim * matDim, sizeof *_tabParents);
 		tabDepth[0] = tabDepth[matDim - 1] = tabDepth[(matDim - 1) * matDim] = tabDepth[matDim * matDim-1] = 0;
 
+		_tabParents[0].x = _tabParents[matDim - 1].x = _tabParents[(matDim - 1) * matDim].x = _tabParents[matDim * matDim-1].x = -1;
+		_tabParents[0].y = _tabParents[matDim - 1].y = _tabParents[(matDim - 1) * matDim].y = _tabParents[matDim * matDim-1].y = -1;
+		_tabParents[0].z = _tabParents[matDim - 1].z = _tabParents[(matDim - 1) * matDim].z = _tabParents[matDim * matDim-1].z = -1;
+		_tabParents[0].w = _tabParents[matDim - 1].w = _tabParents[(matDim - 1) * matDim].w = _tabParents[matDim * matDim-1].w = -1;
+		//tabDepth[matDim / 2 * matDim + matDim / 2] = 9;
 		/*Remplissage mat de parents et profondeur*/
 		initArrays(tabDepth, _tabParents, matDim, matDim);
 
 		/*Affichage profondeur*/
-		// char matProfName[] = "profondeur";
-		// displayMat(tabDepth, matProfName, matDim);	
+		char matProfName[] = "profondeur";
+		displayMat(tabDepth, matProfName, matDim);	
+		//Img output
+
+		Mat img(matDim, matDim, CV_8UC1);
+		const int grayBytes = img.step * img.rows;
+		unsigned char *d_input, *d_img;
+		cudaMalloc(&d_img, nbMatEl* sizeof grayBytes);
+		cudaMemcpy(d_input, img.ptr(), grayBytes, cudaMemcpyHostToDevice);
 
 		cudaMalloc(&devData, nbMatEl* sizeof *devData);
 		cudaMalloc(&devTabDepth, nbMatEl* sizeof *devTabDepth);
 		cudaMalloc(&dev_tabParents, nbMatEl* sizeof *dev_tabParents);
-		cudaMalloc(&d_img, grayBytes);
-
+		
 		cudaMemcpy(devData, data, nbMatEl* sizeof *data, cudaMemcpyHostToDevice);
 		cudaMemcpy(devTabDepth, tabDepth, nbMatEl* sizeof *tabDepth, cudaMemcpyHostToDevice);
 		cudaMemcpy(dev_tabParents, _tabParents, nbMatEl* sizeof *_tabParents, cudaMemcpyHostToDevice);
-		cudaMemcpy(d_input, img.ptr(), grayBytes, cudaMemcpyHostToDevice);
+		
 		
 		int _maxProf = maxProf(tabDepth, matDim);
-		// printf("Max %d\n", _maxProf);
 
 		int th = _maxProf;
+		printf("%d", _maxProf);
 		dim3 dimBlock(th, th, 1);
 		dim3 dimGrid((matDim / dimBlock.x)+1, (matDim / dimBlock.y)+1, 1);
 		
-		initTime();
-		for(int i = 0; i<=_maxProf; ++i){
-			diamont<<<dimGrid, dimBlock, 0>>>(devData, devTabDepth, dev_tabParents, i, matDim);
-			//diamontImg<<<dimGrid, dimBlock, 0>>>(devData, d_img, devTabDepth, dev_tabParents, i, matDim);
+		//initTime();
+		for(int i = 1; i<=_maxProf; i++){
+			diamontImg<<<dimGrid, dimBlock, 0>>>(devData, d_img, devTabDepth, dev_tabParents, i, matDim);
 		}
-		printf("%d\t%F\n", n, getTime());
-		//printf("%d\t time gpu : %F\n", nbMatEl, getTime());
 
 		cudaMemcpy(data, devData, nbMatEl* sizeof *data, cudaMemcpyDeviceToHost);
 		cudaMemcpy(img.ptr(), d_img, grayBytes, cudaMemcpyDeviceToHost);
 
-		//cv::imwrite(filename, img);
+		cv::imwrite(filename, img);
 	}
 
 	// char matDatasName[] = "datas";
 	// displayMat(data, matDatasName, matDim);
+
+	cudaFree(devData); free(data);
+	cudaFree(devTabDepth); free(tabDepth);
 	
 	return 0;
 }
